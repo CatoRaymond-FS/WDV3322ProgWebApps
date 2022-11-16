16b4fe787529fee35e632102f46f9ecefbd4b49d
CatoRaymond-FS <97109752+CatoRaymond-FS@users.noreply.github.com>
2022-11-16 16:18:01 -0500

    Fixed sign up

--- db/db.js
+++ db/db.js
@@ -4,14 +4,13 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 
-//findUser by email address {email: req.body.email}
 const findUser = async (object) => {
-    return await user.findOne({ object});
+    return await user.findOne(object);
+
 };
     
 
 
-//if user exists return 409 message "User already exists"
 const saveUser = async (newUser) => {
     return await newUser.save();
 };
